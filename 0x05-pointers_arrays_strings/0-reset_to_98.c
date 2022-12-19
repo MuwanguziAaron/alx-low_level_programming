060c16555490a94eb226fc607ef6b5f42b9ba311
MuwanguziAaron <aaronmmuwanguzi05@gmail.com>
2022-12-19 03:39:11 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,21 @@
+nclude "main.h"
+
+/**
+ *
+ *  * reset_to_98 - update value of pointer
+ *
+ *   * @n: int to check
+ *
+ *    *
+ *
+ *     * Return: 0 is success
+ *
+ *      */
+
+void reset_to_98(int *n)
+
+{
+
+		*n = 98;
+
+}
